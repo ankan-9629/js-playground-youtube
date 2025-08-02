e5693ee949eb4967a10e32f838a3752c32d0117b
ankan-9629 <reaperoflevels@gmail.com>
2025-08-02 08:29:28 +0000

    Datatypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 122344
+let accountEmail = "ankanbhowmick2222@gmail.com"
+var accountPassword = "12345"
+accountCity = "West Bengal"
+let accountState;
+
+// accountId = 1223
+
+accountEmail = "ab@ab.com"
+accountPassword = "123"
+accountCity = "Tokyo"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS codes as newer version standard
+
+console.log( 3 + 3);
+
+console.log("Ankan");
+
+let age = 21
+let userName = "Ankan"
+let accountLoggedIn = true
+
+console.table([typeof age, typeof userName, typeof accountLoggedIn, typeof null, typeof undefined])
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
