0c9d0393fa62fbe4c6f8dcb37b4f7b097729bc40
ankan-9629 <reaperoflevels@gmail.com>
2025-08-03 10:25:12 +0000

    convesion of datatypes done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+let score1 = "33"
+let score2 = "33ab"
+
+// console.log(typeof score1);
+
+let score1InNumber = Number(score1)
+let score2InNumber = Number(score2)
+
+// console.log(typeof score1InNumber);
+// console.log(score1InNumber);
+// console.log(typeof score2InNumber);
+// console.log(score2InNumber);
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringSomeNumber = String(someNumber)
+
+console.log(typeof stringSomeNumber);
+
