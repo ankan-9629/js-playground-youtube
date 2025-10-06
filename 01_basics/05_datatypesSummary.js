06c16d1696a2414070a3efffcfc8e48831e4a143
ankan-9629 <reaperoflevels@gmail.com>
2025-10-06 08:10:24 +0000

    topic - memory in JS:  added

--- 01_basics/05_datatypesSummary.js
+++ 01_basics/05_datatypesSummary.js
@@ -34,3 +34,22 @@ console.log(typeof myFunction);
 console.log(typeof NaN);
 console.log(typeof anotherId);
 
+// +++++++++++++++++++++++++++++++++++++
+
+let a=5;
+let b=a; // copy of original value
+b=6;
+console.log(a);
+console.log(b);
+
+
+let userOne = {
+    email:"user@gmail.com",
+    upi:"user@sbi"
+};
+
+let userTwo = userOne; // reference of original value
+userTwo.email = "ankanbhowmick@gmail.com";
+
+console.log(userOne);
+console.log(userTwo);
